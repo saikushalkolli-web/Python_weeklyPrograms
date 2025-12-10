5f481d6df67ebd69d59b47c51408ab65afb7fd75
saikushalkolli <saikushal.kolli@gmail.com>
2025-12-10 22:09:14 +0530

    implementation of datatypes

---
+++ week1/DataTypes.py
@@ -0,0 +1,12 @@
+char=input("Enter a string: ")
+print(char,type(char),sep=",")
+num=int(input("Enter a number: "))
+print(num,type(num),sep=",")
+float=float(input("Enter a number: "))      #Float is a datatype in Python but still can be used as a variable
+print(float,type(float),sep=",")
+planet=("Sun","Moon","Earth","Saturn")      #This is a tuple, A tuple can be recognised by ()
+print(list,type(list),sep=",")
+cars={"BMW",'PORSCHE','AUDI','MCLAREN'}     #This is a set, A tuple can be recognised by {}
+print(cars,type(cars),sep=",")
+fruits=["GRAPES",'MANGO','RASPBERRY','PINEAPPLE']       #This is a tuple, A tuple can be recognised by []
+print(fruits,type(fruits),sep=",")
