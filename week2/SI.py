fcdf81b69ed2bf270f2d31706a4c1768059e2e6f
saikushalkolli <saikushal.kolli@gmail.com>
2025-12-11 23:06:16 +0530

    Using Math functions

---
+++ week2/SI.py
@@ -0,0 +1,10 @@
+#24331A05D8
+#fINDING sIMPLE iNTREST
+print("How much Loan do you want?")
+P=int(input())
+print("What is the rate of intrest for your loan?")
+R=int(input())
+print("How much time do you have to repay the loan?(Enter the values in years)")
+T=int(input())
+I=(P*T*R)/100
+print("Your intrest: ",I)
