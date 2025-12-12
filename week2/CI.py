698be1d82287d20d19321825cb8c076417d692b0
saikushalkolli <saikushal.kolli@gmail.com>
2025-12-12 21:46:58 +0530

    Compound Intrest

---
+++ week2/CI.py
@@ -0,0 +1,12 @@
+#24331A05D8
+#fINDING cOMPOUND iNTREST
+print("How much Loan do you want?")
+P=int(input())
+print("What is the rate of intrest for your loan?")
+R=int(input())
+print("How much time do you have to repay the loan?(Enter the values in years)")
+T=int(input())
+I=(P*(1+R/100)**T)-P
+print("Your total intrest to be paid: ",round(I,2))
+
+
