18d6a500e2dba989585bd290e5a0436469f01893
saikushalkolli-web <saikushalkolli@gmail.com>
2025-12-14 20:25:23 +0530

    Concatnation of Strings

---
+++ week2/concat.py
@@ -0,0 +1,8 @@
+#24331A05D8
+#CONCATENATION OF STRINGS
+word=input("Enter a word to display them in Upper case: ")
+word=word.upper()                                           #.upper() is the function used to concatenate strings into upper case.
+print("Given word in upper case: ",word)
+char=input("Enter another word to display them in lower case: ")
+char=char.lower()                                           #.lower() is the function used to concatenate strings into lower case.
+print("Given word in lower case: ",char)
