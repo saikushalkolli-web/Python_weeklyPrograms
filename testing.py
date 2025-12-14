e9c3871a60ad769851fa859b91a6679e1a749ef2
Sai_Kushal <saikushal.kolli@gmail.com>
2025-12-14 19:36:21 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-num=int(input("Enter a number:"))
-print(num,type(num),sep=",")
-num=str(num)                                     #conversion of int datatype to string datatype
-print("After typeconversion: ",type(num))
-char=input("Enter a string:")
-print(char,type(char),sep=",")
-char=int(char)                                  #conversion of string datatype to int datatype
-print("After typeconversion: ",type(char))
