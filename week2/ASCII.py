b863c7e6b12b3d000d36e4a95e45d2dd38851b85
Sai_Kushal <saikushal.kolli@gmail.com>
2025-12-14 19:46:35 +0530

    Delete week2/ASCII.py

--- week2/ASCII.py
+++
@@ -1,5 +0,0 @@
-char=input("Enter a charecter: ")
-print("ASCII value of {} is:{}".format((char),ord(char)))
-if len(char) >=2 or len(char)<=0:
-    print("Invalid inputs give")
-print("Exiting the loop!!!")
