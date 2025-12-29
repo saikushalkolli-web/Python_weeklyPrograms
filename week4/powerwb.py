9cf7c66752db843a57cda0a5e6d41c090a7daef8
saikushalkolli-web <saikushalkolli@gmail.com>
2025-12-29 22:13:14 +0530

    Power without built-in functions

---
+++ week4/powerwb.py
@@ -0,0 +1,5 @@
+#24331a05d8
+#Finding power of the number without using builtin functions.
+n=int(input("Enter exponent value: "))
+m=int(input("Enter base value: "))
+print("Value of",m,"to the power of",n,"is: ",m**n)
