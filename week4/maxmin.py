2cc0724f8ce998316b76da622964891eec31cd01
saikushalkolli-web <saikushalkolli@gmail.com>
2026-01-02 21:49:59 +0530

    finding MAX/MIN values without built-in functions

--- week4/maxmin.py
+++ week4/maxmin.py
@@ -1,3 +1,5 @@
+#24331A05D8
+#fINDING mAXIMUM aND mINIMUM
 a=int(input("Enter value of A"))
 b=int(input("Enter value of B"))
 c=int(input("Enter value of C"))
@@ -9,3 +11,9 @@ else:
     print("{} is the largest number".format(c))
 if(a==b and b==c):
     print("All the numbers have same values")
+if(a<b and b<c):
+    print("{} is the smallest number".format(a))
+elif(b<a and b<c):
+    print("{} is the smallest number".format(b))
+else:
+    print("{} is the smallest number".format(c))
