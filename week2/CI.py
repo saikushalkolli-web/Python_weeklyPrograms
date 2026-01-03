24564a926d6cf64a26caad24edeb942ccbb1769f
saikushalkolli-web <saikushalkolli@gmail.com>
2026-01-03 20:49:36 +0530

    Compound Interest

--- week2/CI.py
+++ week2/CI.py
@@ -6,7 +6,8 @@ print("What is the rate of intrest for your loan?")
 R=int(input())
 print("How much time do you have to repay the loan?(Enter the values in years)")
 T=int(input())
-I=(P*(1+R/100)**T)-P
+n=int(input("How many times the interest is being compounded per year?"))
+I=(P*(1+R/(100*n))**(T*n))-P
 print("Your total intrest to be paid: ",round(I,2))
 
 
