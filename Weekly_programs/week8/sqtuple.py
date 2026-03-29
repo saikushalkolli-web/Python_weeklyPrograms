a29ef511eabdd0d55882d917731bdb2b77e56db2
Sai_Kushal <saikushal.kolli@gmail.com>
2026-03-29 10:03:01 +0530

    Delete Weekly_programs/week8/sqtuple.py

--- Weekly_programs/week8/sqtuple.py
+++
@@ -1,6 +0,0 @@
-#24331A05D8
-#Program to show the number and it's square in the same list.
-first=int(input("Enter the first number: "))
-last=int(input("Enter the last number: "))
-new_tup=[(i,i**2)for i in range(first,last+1)]
-print("New tuple list: ",new_tup)
